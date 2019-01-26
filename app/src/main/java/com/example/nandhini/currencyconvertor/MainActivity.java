60156950fa501c140fac51ae5c1d2c3aafd71e49
unknown <nandhiniraja208@gmail.com>
2019-01-26 06:44:44 +0530

    Adding files to git

---
+++ app/src/main/java/com/example/nandhini/currencyconvertor/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.nandhini.currencyconvertor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void calc(View view){
+
+        EditText cur=(EditText) findViewById(R.id.editText);
+        Double neww = Double.parseDouble(cur.getText().toString());
+        Double convert=neww*65;
+        Toast.makeText(this,convert.toString(), Toast.LENGTH_SHORT).show();
+
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
